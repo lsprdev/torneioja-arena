0673996b4a08534e9de1df8fc78466cd8197b00f
lsprdev <github@lspr.dev>
2024-09-24 20:32:00 -0300

    Criando Register

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -15,9 +15,9 @@ export function LoginForm() {
   return (
     <Card className="mx-auto max-w-sm">
       <CardHeader>
-        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardTitle className="text-2xl">Entrar</CardTitle>
         <CardDescription>
-          Enter your email below to login to your account
+          Preencha os campos abaixo para acessar sua conta.
         </CardDescription>
       </CardHeader>
       <CardContent>
@@ -27,7 +27,7 @@ export function LoginForm() {
             <Input
               id="email"
               type="email"
-              placeholder="m@example.com"
+              placeholder="joao@example.com"
               required
             />
           </div>
@@ -35,22 +35,23 @@ export function LoginForm() {
             <div className="flex items-center">
               <Label htmlFor="password">Password</Label>
               <Link href="#" className="ml-auto inline-block text-sm underline">
-                Forgot your password?
+                Esqueceu sua senha?
               </Link>
             </div>
-            <Input id="password" type="password" required />
+            <Input 
+              id="password" 
+              type="password" 
+              placeholder="********"
+              required />
           </div>
           <Button type="submit" className="w-full">
-            Login
-          </Button>
-          <Button variant="outline" className="w-full">
-            Login with Google
+            Entrar
           </Button>
         </div>
         <div className="mt-4 text-center text-sm">
-          Don&apos;t have an account?{" "}
+          Não tem uma conta?{" "}
           <Link href="#" className="underline">
-            Sign up
+            Cadastre-se
           </Link>
         </div>
       </CardContent>
---
+++ src/components/register-form.tsx
@@ -0,0 +1,70 @@
+import Link from "next/link"
+
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function RegisterForm() {
+    return (
+        <Card className="mx-auto max-w-sm">
+            <CardHeader>
+                <CardTitle className="text-2xl">Cadastro</CardTitle>
+                <CardDescription>
+                    Preencha os campos abaixo para criar sua conta.
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div className="grid gap-4">
+                    <div className="grid gap-2">
+                        <Label htmlFor="email">Nome</Label>
+                        <Input
+                            id="name"
+                            type="name"
+                            placeholder="João da Silva"
+                            required
+                        />
+                    </div>
+                    <div className="grid gap-2">
+                        <Label htmlFor="email">Email</Label>
+                        <Input
+                            id="email"
+                            type="email"
+                            placeholder="joao@example.com"
+                            required
+                        />
+                    </div>
+                    <div className="grid gap-2">
+                        <div className="flex items-center">
+                            <Label htmlFor="password">Senha</Label>
+
+                        </div>
+                        <Input 
+                            id="password" 
+                            type="password" 
+                            placeholder="********"
+                            required />
+                    </div>
+                    <Button type="submit" className="w-full">
+                        Criar conta
+                    </Button>
+                    {/* <Button variant="outline" className="w-full">
+                        Login with Google
+                    </Button> */}
+                </div>
+                <div className="mt-4 text-center text-sm">
+                    Já tem uma conta?{" "}
+                    <Link href="#" className="underline">
+                        Entrar
+                    </Link>
+                </div>
+            </CardContent>
+        </Card>
+    )
+}

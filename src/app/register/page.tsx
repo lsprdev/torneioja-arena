4b9c7fe4332a8ccbc74d16b7988eb0d775fc397f
lsprdev <github@lspr.dev>
2024-09-24 21:34:13 -0300

    Mudando posição da logo

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,15 +4,13 @@ import { CalendarDays } from "lucide-react"
 export default function Page() {
     return (
         <>
-            <div className="flex-col h-screen w-full justify-center px-4">
-                <div className="flex h-14 w-full bg-black items-center px-20">
-                    <span className="flex">
+            <div className="flex h-screen w-full items-center justify-center px-4">
+                <div>
+                    <RegisterForm />
+                    <span className="flex items-center justify-center my-8">
                         <CalendarDays size={29} className="text-white" />
                         <h1 className={`${bebasNeue.className} text-white text-2xl mx-1`}>Arena Já</h1>
                     </span>
-                </div>
-                <div className="flex h-3/4 w-full items-center justify-center px-4">
-                    <RegisterForm />
                 </div>
             </div>
         </>

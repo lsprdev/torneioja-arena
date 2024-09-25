6701da81bc4cd587c23196af548bfa534b529afe
lsprdev <github@lspr.dev>
2024-09-25 08:45:49 -0300

    Implementando Dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,24 +1,33 @@
-'use client'
-import { useEffect } from 'react';
+import { Button } from "@/components/ui/button"
+import { CalendarDays, LayoutDashboard, Home } from "lucide-react"
+import { bebasNeue } from "../fonts/fonts"
 
 export default function Page() {
-
-    useEffect(() => {
-        // Verifica se o token existe
-        if (!localStorage.getItem('arena_token')) {
-            // Redireciona para a página de login se o token não existir
-            window.location.href = '/login';
-        }
-    });
-
-
     return (
-        <>
-            <div className="flex h-screen w-full items-center justify-center px-4">
-            <div>
-                <h1 className="text-white text-2xl mx-1">Dashboard</h1>
-            </div>
-            </div>
-        </>
+        <div className="flex h-screen bg-gray-100">
+            {/* Sidebar */}
+            <aside className="w-64 bg-white shadow-md">
+                <div className="flex p-4">
+                    <CalendarDays size={29} className="text-black" />
+                    <h1 className={`${bebasNeue.className} text-2xl font-bold text-primary mx-1`}>Arena Já</h1>
+                </div>
+                <nav className="mt-8">
+                    <Button variant="ghost" className="w-full justify-start px-4 py-2 text-left text-black">
+                        <LayoutDashboard className="mr-2 h-4 w-4" />
+                        Visão geral
+                    </Button>
+                    <Button variant="ghost" className="w-full justify-start px-4 py-2 text-left text-black">
+                        <Home className="mr-2 h-4 w-4" />
+                        Minha arena
+                    </Button>
+                </nav>
+            </aside>
+
+            {/* Main content area */}
+            <main className="flex-1 p-8">
+                <h2 className="text-2xl font-semibold mb-4 text-black">Bem-vindo à sua Arena</h2>
+                <p className="text-black">Selecione uma opção no menu lateral para começar.</p>
+            </main>
+        </div>
     )
 }
